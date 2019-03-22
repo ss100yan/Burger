77e59b7aa639896a73d8e7c873abddbd6cd3b9aa
ss100yan <32798618+ss100yan@users.noreply.github.com>
2019-03-22 02:29:16 -0400

    new

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,9 +1,45 @@
-// * Express
-// * `burger.js`
+// importing  Express and burger.js
+    var express = require('express');
+       var burger = require('../models/burger.js');
 
-var express = require('express');
-var burger = require('../models/burger.js')
+// Create the `router` for the app.  
+   var router = express.Router();
+
+//create the main router
+router.get('/',function(req,res){
 
 
+   //create an object that holds information from both the burger and menu table
+    var info = {
+      brgr: [],
+      // itm: []
+    };
 
-// 4. Create the `router` for the app, and export the `router` at the end of your file.
+// grab data from burger table
+    burger.selectAll(function(data){
+      for(var i=0;i<data.length; i++){
+        info.brgr.push(data[i]);
+      }
+   });    
+   
+  
+//render index.handlebars
+    res.render('index',info)
+});
+
+  router.post('/create', function(req, res){
+    burger.insertOne([req.body.burgerInput], function(){
+      res.redirect('/');
+    });
+  });
+  
+  router.put('/update/:id', function(req, res){
+    burger.updateOne([req.body.devoured], [req.params.id], function(){
+      res.redirect('/');
+    });
+  });
+  
+
+
+  // export the `router` at the end of your file.
+  module.exports = router;
--- server.js
+++ server.js
@@ -5,8 +5,40 @@ var path = require('path');
 
 var app = express();
 
-var PORT = process.env.PORT || 5000;
 
-app.get('/',(req, res) => res.send('INDEX'));
 
-app.listen(PORT,console.log(`server started on port ${PORT}`));
+
+//  static content from /public.
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+
+
+
+// settig up handlebars engine, views folder
+  app.set('views', path.join(__dirname, 'views'));
+    app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+      app.set('view engine', 'handlebars');
+
+//imporing the routing(routes) from burgers_controller.js
+   var routes = require('./controllers/burgers_controller.js');
+
+//seting up the main ('/') route
+   app.use('/', routes);
+
+//rooting to index.handlebars
+// app.get('/',function(req,res){
+//     res.render('index')
+// });
+
+app.set('port',(process.env.PORT || 5000));
+
+app.listen(app.get('port'),function(){console.log('Server started on port' +app.get('port'))});
+
+
+
+
+
