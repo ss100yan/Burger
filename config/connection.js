6deedb6398316c95d6bec2f2c1eca59ba4a21944
ss100yan <32798618+ss100yan@users.noreply.github.com>
2019-03-23 23:27:59 -0400

    new

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 
 var mysql = require ('mysql');
 
-var connection = mysql.createConnection({
-host : 'localhost',
-database : 'burger_db',
-user : 'root',
-password : 'root',
-});
+var connection;
+// creatinng connection to mySQL or JAWSDB for deploing to HEROKUadd 
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+  connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'burger_db'
+  });
+};
 
 connection.connect(function(err) {
     if (err) {
